a804c6e126e3082c9f2ebcf47ac83af3017af875
Neil A Jerome <neilanthony.jerome@rakuten.com>
2020-07-08 16:09:31 +0900

    Added problems for empty interfaces and type checking

---
+++ 202007/007-empty-interface-and-type-assertion/empty_interface.go
@@ -0,0 +1,54 @@
+package emptyint
+
+// MultitypeSum adds a list of different types of numbers together, producing a float64.
+// It can handle the following types:
+//			float64, int64, uint64
+// If the input list contains any other type, that element of the list is ignored.
+// If the list is empty (or consists of only non-supported types) returns 0.0
+func MultitypeSum(input []interface{}) float64 {
+	return 0.0 // TODO implement
+}
+
+// HasStackTrace is an interface matching any error that has a stack trace associated with it
+type HasStackTrace interface {
+
+	// GetStackTrace gets the error's stack trace
+	GetStackTrace() string
+
+	// SetStackTrace set's the error's stack trace
+	SetStackTrace(st string)
+
+	// Error gets the message of the root error (excluding stack trace)
+	Error() string
+}
+
+// A simple implementation of HasStackTrace
+type stackTraceError struct {
+	err        error
+	stackTrace string
+}
+
+func (e *stackTraceError) Error() string {
+	return e.err.Error()
+}
+
+func (e *stackTraceError) GetStackTrace() string {
+	return e.stackTrace
+}
+
+func (e *stackTraceError) SetStackTrace(st string) {
+	e.stackTrace = st
+}
+
+// AddStackTrace either adds a stack trace to an error which does not have one (by returning a stackTraceError),
+// or adds another line to the stack trace of an error that does have one already.
+// If the input err is nil, returns nil
+// The stacktrace format is:
+//			"${stack line} :: ${current stack trace of err}"
+// Or if error does not yet have a stack trace:
+// 			"${stack line} :: ${message of err}"
+// For updating an existing stack trace, this method should work with input of any type that matches
+// HasStackTrace, not only stackTraceError.
+func AddStackTrace(err error, stackLine string) error {
+	return nil // TODO implement
+}
---
+++ 202007/007-empty-interface-and-type-assertion/empty_interface_test.go
@@ -0,0 +1,104 @@
+package emptyint
+
+import (
+	"../testutil"
+	"errors"
+	"testing"
+)
+
+// Tests for MultitypeSum
+
+func TestMultitypeSum_EmptyListInput_ReturnsZero(t *testing.T) {
+	res := MultitypeSum([]interface{}{})
+
+	testutil.AssertEqual(t, 0.0, res)
+}
+
+func TestMultitypeSum_AllSupportedTypes_ReturnsSumOfAllInput(t *testing.T) {
+	res := MultitypeSum([]interface{}{0.11, uint64(1), int64(2)})
+
+	exptd := 0.11 + float64(uint64(1)) + float64(int64(2))
+	testutil.AssertEqual(t, exptd, res)
+}
+
+func TestMultitypeSum_IncludingUnsupportedTypes_ReturnsSumExcludingUnsupported(t *testing.T) {
+	res := MultitypeSum([]interface{}{0.11, []int{1}, float32(0.22), uint64(1), int64(2), "dfhsdf"})
+
+	exptd := 0.11 + float64(uint64(1)) + float64(int64(2))
+	testutil.AssertEqual(t, exptd, res)
+}
+
+func TestMultitypeSum_AllUnsupportedTypes_ReturnsZero(t *testing.T) {
+	res := MultitypeSum([]interface{}{[]int{1}, float32(0.22), "dfhsdf"})
+
+	testutil.AssertEqual(t, 0.0, res)
+}
+
+// tests for AddStackTrace
+
+func TestAddStackTrace_Nil_ReturnsNil(t *testing.T) {
+	res := AddStackTrace(nil, "stacked")
+
+	testutil.AssertErrNil(t, res, true)
+}
+
+func TestAddStackTrace_RegularError_AddsStackTrace(t *testing.T) {
+	input := errors.New("bigolerror")
+
+	res := AddStackTrace(input, "stacked")
+
+	hasStack, ok := res.(HasStackTrace)
+	if ok {
+		testutil.AssertEqual(t, input.Error(), hasStack.Error())
+		testutil.AssertEqual(t, "stacked :: bigolerror", hasStack.GetStackTrace())
+		return
+	}
+	t.Fatal("Did not return an implementation of HasStackTrace")
+}
+
+func TestAddStackTrace_stackTraceError_AddsNewStackLine(t *testing.T) {
+	theErr := errors.New("bigolerror")
+	input := &stackTraceError{err: theErr, stackTrace: "st1 :: st0"}
+
+	res := AddStackTrace(input, "st2")
+
+	hasStack, ok := res.(HasStackTrace)
+	if ok {
+		testutil.AssertEqual(t, input.Error(), hasStack.Error())
+		testutil.AssertEqual(t, "st2 :: st1 :: st0", hasStack.GetStackTrace())
+		return
+	}
+	t.Fatal("Did not return an implementation of HasStackTrace")
+}
+
+type otherImpl struct {
+	err        error
+	stackTrace string
+}
+
+func (e *otherImpl) Error() string {
+	return e.err.Error()
+}
+
+func (e *otherImpl) GetStackTrace() string {
+	return e.stackTrace
+}
+
+func (e *otherImpl) SetStackTrace(st string) {
+	e.stackTrace = st
+}
+
+func TestAddStackTrace_OtherImplOfHasStackTrace_AddsNewStackLine(t *testing.T) {
+	theErr := errors.New("bigolerror")
+	input := &otherImpl{err: theErr, stackTrace: "st1 :: st0"}
+
+	res := AddStackTrace(input, "st2")
+
+	hasStack, ok := res.(HasStackTrace)
+	if ok {
+		testutil.AssertEqual(t, input.Error(), hasStack.Error())
+		testutil.AssertEqual(t, "st2 :: st1 :: st0", hasStack.GetStackTrace())
+		return
+	}
+	t.Fatal("Did not return an implementation of HasStackTrace")
+}
