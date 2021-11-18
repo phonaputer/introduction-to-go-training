d2a1068657e474fddefdade8550e671fba1b7ae7
Neil <neilanthony.jerome@rakuten.com>
2021-11-18 15:20:19 +0900

    MySQL connection

--- intro_to_go_codealong/go.mod
+++ intro_to_go_codealong/go.mod
@@ -1,3 +1,5 @@
 module intro_to_go_codealong
 
 go 1.17
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
--- intro_to_go_codealong/main.go
+++ intro_to_go_codealong/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	db, err := sql.Open("mysql", "docker:docker@tcp(127.0.0.1)/introtogo")
+	if err != nil {
+			panic(err)
+	}
+
+	row := db.QueryRow("SELECT id, first_name, middle_name, last_name, age FROM customers WHERE id=?", 11)
+
+	var id, age int
+	var firstName, lastName string
+	var middleName *string
+
+	err = row.Scan(&id, &firstName, &middleName, &lastName, &age)
+	if err != nil {
+		panic(err)
+	}
+
+	result := fmt.Sprintf("%v: %s ", id, firstName)
+
+	if middleName != nil {
+		result += *middleName + " "
+	}
+
+	result += fmt.Sprintf("%s. Age: %v ", lastName, age)
+
+	fmt.Println(result)
 }
 
