ded2032782841b1607f529e93ed014228edc6479
Neil <neilanthony.jerome@rakuten.com>
2021-11-18 15:33:38 +0900

    POC HTTP server returning MySQL data

--- intro_to_go_codealong/main.go
+++ intro_to_go_codealong/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"net/http"
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/customers", http.HandlerFunc(pocHandler))
+
+	server := &http.Server{Addr: ":8080", Handler: mux}
+
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		panic(err)
+	}
+}
+
+func pocHandler(w http.ResponseWriter, r *http.Request) {
+	userData := getFromMySQL()
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(userData))
+}
+
+func getFromMySQL() string {
 	db, err := sql.Open("mysql", "docker:docker@tcp(127.0.0.1)/introtogo")
 	if err != nil {
 			panic(err)
 	}
+	defer db.Close()
 
 	row := db.QueryRow("SELECT id, first_name, middle_name, last_name, age FROM customers WHERE id=?", 11)
 
@@ -31,6 +52,6 @@ func main() {
 
 	result += fmt.Sprintf("%s. Age: %v ", lastName, age)
 
-	fmt.Println(result)
+	return result
 }
 
