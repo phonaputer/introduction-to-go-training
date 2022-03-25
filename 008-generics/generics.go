5c6d72ad34f887c917cdc9e3d826042636ba53dc
Neil <neilanthony.jerome@rakuten.com>
2022-03-25 13:55:21 +0900

    Move channels from 008 to 009. Create 008 for generics

---
+++ 008-generics/generics.go
@@ -0,0 +1,8 @@
+package generics
+
+// Want to test:
+//
+// 1. Knowledge of calling generic functions (with & without type arguments)
+// 2. Knowledge of writing new generic functions using type constraints as needed
+// 3. Knowledge of using generic types
+// 4. Knowledge of writing new generic types (perhaps implementing generic interfaces)
