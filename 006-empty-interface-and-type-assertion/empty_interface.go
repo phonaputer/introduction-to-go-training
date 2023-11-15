0f8f7a3a56ab39891b0a9e69d73f521e5b8f17f3
rafisudrajat <rafisudrajat.99@gmail.com>
2023-11-15 21:17:12 +0900

    Solve problems 006

--- 006-empty-interface-and-type-assertion/empty_interface.go
+++ 006-empty-interface-and-type-assertion/empty_interface.go
@@ -2,11 +2,25 @@ package emptyint
 
 // MultitypeSum adds a list of different types of numbers together, producing a float64.
 // It can handle the following types:
-//			float64, int64, uint64
+//
+//	float64, int64, uint64
+//
 // If the input list contains any other type, that element of the list is ignored.
 // If the list is empty (or consists of only non-supported types) returns 0.0
 func MultitypeSum(input []interface{}) float64 {
-	return 0.0 // TODO implement
+	var result float64 = 0.0
+	for _, element := range input {
+		switch concreteValue := element.(type) {
+		case float64:
+			result += concreteValue
+		case int64:
+			result += float64(concreteValue)
+		case uint64:
+			result += float64(concreteValue)
+
+		}
+	}
+	return result
 }
 
 // Stringer is an interface for something which can be represented as a string
@@ -17,10 +31,24 @@ type Stringer interface {
 	String() string
 }
 
+type ConcreteStringer struct {
+	stringRepresentation string
+}
+
+func (concreteStringer *ConcreteStringer) String() string {
+	return concreteStringer.stringRepresentation
+}
+
 // AppendIfStringer checks if "input" is an implementation of Stringer and, if yes, returns a new Stringer
 // with the value of "str" appended to the output of input.String():
-//		inputIfStringer.String() + str
+//
+//	inputIfStringer.String() + str
+//
 // If "input" is not a Stringer, returns a new Stringer containing only the value of "str".
 func AppendIfStringer(input interface{}, str string) Stringer {
-	return nil // TODO implement
+	inputStringer, ok := input.(Stringer)
+	if ok {
+		return &ConcreteStringer{inputStringer.String() + str}
+	}
+	return &ConcreteStringer{str}
 }
